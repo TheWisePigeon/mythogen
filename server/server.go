0f368b80091de10f8c39ba904b716009a1069458
TheWisePigeon <zozozozeph@gmail.com>
2023-06-19 21:25:44 +0000

    minor updates

--- server/server.go
+++ server/server.go
@@ -5,11 +5,14 @@ import (
     "github.com/gofiber/fiber/v2"
 )
 
-func Serve( port string ){
+func Serve( port *string ){
     mythogen := fiber.New()
-    fmt.Printf("Mythogen server launched on port %s", port)
+    fmt.Printf("Mythogen server launched on port %s", *port)
     mythogen.Get("/test", func(c *fiber.Ctx) error {
         return c.SendString("Hello from Mythogen server")
     })
-    mythogen.Listen(fmt.Sprintf(":%s", port))
+    err := mythogen.Listen(fmt.Sprintf(":%s", *port))
+    if err != nil {
+        panic(err)
+    }
 }
